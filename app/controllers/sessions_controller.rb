9f2412f7b8cd397bad369fc1c73402de4a834de5
Yankel <yankelamarante@gmail.com>
2017-05-11 12:38:35 -0400

    loggin in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  # def show
+  # end
+
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+    if @user.nil?
+      return redirect_to new_users_path
+    else
+      return redirect_to expense_categories_path if @user
+    end
+    # session[:user_id] = @user.id
+
+
+    # logged in, hooray
+
+  end
 end
