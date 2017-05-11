7fc8e569409c603cba897ba9c82d32404e423c92
Yankel <yankelamarante@gmail.com>
2017-05-11 12:36:02 -0400

    start sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  resource :sessions
   resource :users
-  get 'users/new'
+  # get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
