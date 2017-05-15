fa9243a3c2550feaeab91d7fb2c46f9c9ab732a8
Yankel <yankelamarante@gmail.com>
2017-05-15 17:02:37 -0400

    home page changes depending on user login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+  # before_action :autheticated
 
 
   def current_user
@@ -8,12 +9,21 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
+    # not @current_user.nil? # was !!current_user
     !!current_user
   end
 
   def require_logged_in
-    if !logged_in?
+    if !logged_in? #was !logged_in?
       redirect_to new_sessions_path, :notice => "Log in please!"
     end
   end
+
+  # def autheticated
+  #   if logged_in?
+  #     redirect_to expense_categories_path
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
 end
