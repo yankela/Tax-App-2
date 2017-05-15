d2fbef545d3dec76f83d397172a905bf2c6ef788
William Bright <williambright91@gmail.com>
2017-05-15 09:47:44 -0400

    added sessions routes, configured App controller for sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
+
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def logged_in?
+    !!current_user
+  end
+
   def require_logged_in
-    return true if current_user
-    return redirect_to root_path
+    if !logged_in?
+      redirect_to new_sessions_path, :notice => "Log in please!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :expense_categories
   root 'users#home'
 
-  resource :sessions
+  resources :sessions, only: [:create, :new, :destoy]
   resource :users
+  get '/logout', to: 'sessions#destroy'
   # get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
