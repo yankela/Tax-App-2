2c63ee979e0328dbfed62200fa45f01baf196b2c
Yankel <yankelamarante@gmail.com>
2017-05-21 12:25:58 -0400

    removed extra buttons from login/signup

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,15 @@
-function ready() {
+ function ready() {
 
-   // auto-focus the task_name input box on every page load
-   // $('#task_name').focus();
 
-   // submit the edit_task form when the "primary" button is clicked
-   $('.modal-footer .btn-primary').click(function() {
-     $('#new_user').submit();
-   });
- }
+    // auto-focus the task_name input box on every page load
+    // $('#task_name').focus();
+
+    // submit the edit_task form when the "primary" button is clicked
+    $('.modal-footer .btn-primary').click(function() {
+      $('#new_user').submit();
+    });
+  }
+
+  // Rails/Turbolinks ready function
+  //   works on Turbolinks/AJAX page loads
+  $(document).on('turbolinks:load', ready);
