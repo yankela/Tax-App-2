574798673f36ae4cbebe88c73e08ec05357a0e28
Yankel <yankelamarante@gmail.com>
2017-05-21 13:51:44 -0400

    added otf file for LemonMilk

--- app/assets/javascripts/expense_categories.js
+++ app/assets/javascripts/expense_categories.js
@@ -13,3 +13,19 @@ function ready() {
 // Rails/Turbolinks ready function
 //   works on Turbolinks/AJAX page loads
 $(document).on('turbolinks:load', ready);
+
+function ready() {
+
+
+   // auto-focus the task_name input box on every page load
+   // $('#task_name').focus();
+
+   // submit the edit_task form when the "primary" button is clicked
+   $('.modal-footer .btn-primary').click(function() {
+     $('#show_receipt').submit();
+   });
+ }
+
+ // Rails/Turbolinks ready function
+ //   works on Turbolinks/AJAX page loads
+ $(document).on('turbolinks:load', ready);
