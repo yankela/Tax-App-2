360030872b8649b9c39134c91f28bc3779f66604
William Bright <williambright91@gmail.com>
2017-05-22 19:22:05 -0400

    commented out methods for wicked

--- app/helpers/wicked_pdf_helper.rb
+++ app/helpers/wicked_pdf_helper.rb
@@ -1,13 +1,14 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
-     sources.collect { |source|
-       asset = (Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)).find_asset("#{source}.css").to_s
-
-       if asset.nil?
-         raise "could not find asset for #{source}.css"
-       else
-         "<style type='text/css'>#{asset}</style>"
-       end
-     }.join("\n").gsub(/url\(['"](.+)['"]\)(.+)/,%[url("#{wicked_pdf_image_location("\\1")}")\\2]).html_safe
+    #  sources.collect { |source|
+    #    asset = (Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)).find_asset("#{source}.css").to_s
+     #
+    #    if asset.nil?
+    #      raise "could not find asset for #{source}.css"
+    #    else
+    #      "<style type='text/css'>#{asset}</style>"
+    #    end
+    #  }.join("\n").gsub(/url\(['"](.+)['"]\)(.+)/,%[url("#{wicked_pdf_image_location("\\1")}")\\2]).html_safe
+    ""
    end
 end
