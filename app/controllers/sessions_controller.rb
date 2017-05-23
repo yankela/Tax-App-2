c1435937c231a4823c518dc248e5297dee798fff
William Bright <williambright91@gmail.com>
2017-05-23 15:41:18 -0400

    added route for possible wrong password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
     if @user.nil?
       return redirect_to root_path, notice: "Please create an account."
+    elsif !@user
+      return redirect_to root_path, notice: "Wrong Password "
     else
       session[:user_id] = @user.id
       return redirect_to expense_categories_path if @user
