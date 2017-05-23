30bc342eb673d5eebf364db406dec7dc70f81462
William Bright <williambright91@gmail.com>
2017-05-23 16:03:34 -0400

    deleted algolia from models in expense categories

--- app/models/expense_category.rb
+++ app/models/expense_category.rb
@@ -1,10 +1,7 @@
 class ExpenseCategory < ApplicationRecord
   belongs_to :user
   has_many :receipts, dependent: :delete_all
-  include AlgoliaSearch
 
-    algoliasearch do
-      
-    end
+
   # has_many :receipts, dependent: destroy
 end
