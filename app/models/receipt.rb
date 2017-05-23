15fa48bd38b592fd9a4bd9aaa523de211c1985ce
William Bright <williambright91@gmail.com>
2017-05-23 17:18:15 -0400

    added back algolia withou asynch

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -3,10 +3,10 @@ include AlgoliaSearch
   belongs_to :expense_category#, touch: true
 
 
-      algoliasearch auto_index: false, auto_remove: false do
-      attribute :store_name, :text_response, :comments, :created_at, :total, :store_name, :address, :userid
-      attributesForFaceting [:userid]
-    end
+  algoliasearch  do
+    attribute :store_name, :text_response, :comments, :created_at, :total, :store_name, :address, :userid
+    attributesForFaceting [:userid]
+  end
 
 
   validates :picture,
