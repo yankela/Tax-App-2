f1056d42b534fc8a8c6ae548b41f2df31cb39e6b
William Bright <williambright91@gmail.com>
2017-05-23 20:34:04 -0400

    added Receipt.reindex to expense controller

--- app/controllers/expense_categories_controller.rb
+++ app/controllers/expense_categories_controller.rb
@@ -71,6 +71,7 @@ class ExpenseCategoriesController < ApplicationController
   # DELETE /expense_categories/1.json
   def destroy
     @expense_category.destroy
+    Receipt.reindex
     respond_to do |format|
       format.html { redirect_to expense_categories_url}
       format.json { head :no_content }
